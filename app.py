91aee8dcab9f82744fea31ec8f10996f6b1ff70d
Mark Daniels <mark_daniels1982@hotmail.com>
2015-10-19 11:10:40 -0700

    add user auth

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 
 app.secret_key = "lotion in the basket"
 
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 
 @app.route('/')
+@login_required
 def home():
-    return "Hello, world!"
+    return render_template("index.html")
 
 
 @app.route('/welcome')
@@ -22,13 +35,15 @@ def login():
              error = 'Invalid credentials. Please try again.'
          else:
             session['logged_in'] = True
-            flash ("You were just logged_in")
+            flash ("You were just logged in!")
             return redirect(url_for('home'))
      return render_template('login.html', error=error)
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('logged_in', None)
+    flash ("You were just logged out!")
     return redirect(url_for('welcome'))
 
 
